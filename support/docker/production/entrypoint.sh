700bcbce9fa55ea5c0365369338649b561f20df6
Zoey <zoey@z0ey.de>
2024-01-28 10:35:24 +0100

    use su-exec on alpine and gosu on debian

--- support/docker/production/entrypoint.sh
+++ support/docker/production/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-chown -R peertube:peertube /config
+find /config ! -user peertube -exec chown peertube:peertube {} \; || true
 
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
@@ -10,9 +10,15 @@ if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
 fi
 
 # allow the container to be started with `--user`
-if [ "$1" = 'node' -a "$(id -u)" = '0' ]; then
-    chown -R peertube:peertube /data
-    exec su peertube "$0" "$@"
+if [ "$1" = 'node' ] && [ "$(id -u)" = '0' ]; then
+    find /data ! -user peertube -exec chown peertube:peertube {} \;
+    if command -v su-exec >/dev/null; then
+      exec su-exec peertube "$0" "$@"
+    elif command -v gosu >/dev/null; then
+      exec gosu peertube "$0" "$@"
+    else
+      exec su peertube "$0" "$@"
+    fi
 fi
 
 exec "$@"
